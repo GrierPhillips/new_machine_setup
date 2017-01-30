8a9a485e113a6a87c9c569e6411aa0b775cfa02b
grierphillips <grierphillips@gmail.com>
2017-01-30 10:11:10 -0700

    Added some new packages and apps to install, included install for Atom and Flux, aliased hub with git

--- new_machine_setup.sh
+++ new_machine_setup.sh
@@ -1,3 +1,5 @@
+# This script was adapted from Miles Erickson's setup.sh
+
 # Change permissions on /usr/local to User
 sudo chown -R $LOGNAME /usr/local
 
@@ -13,8 +15,33 @@ conda update anaconda
 rm anaconda_script.sh
 source ~/.bash_profile
 
+# Install useful Mac Apps
+brew tap caskroom/cask
+brew cask install mou
+brew cask install iterm2
+brew cask install xquartz
+brew cask install virtualbox
+brew cask install vagrant
+brew cask install postgres
+brew cask install pgadmin3
+brew cask install gephi
+brew cask install java
+# To Do: Consider changing editors! VSCode, Sublime, or just VIM
+
+echo 'export PATH="~/Applications/Postgres.app/Contents/Versions/latest/bin/:$PATH"' >> ~/.bash_profile
+
+## Mac Packages
+brew install wget
+brew install mongodb
+brew install graphviz
+brew install imagemagick
+brew install terminal-notifier
+brew install gcc
+
 # Install some useful packages to python
 conda install --yes statsmodels
+conda install --yes networkx
+conda install --yes pymc
 pip install pymongo
 conda install -c https://conda.binstar.org/trent psycopg2
 pip install unittest2
@@ -28,6 +55,9 @@ brew install pig
 brew install hive
 brew install git
 brew install --HEAD hub
+
+# Alias hub with git in bash_profile
+echo  "alias git=hub >> ~/.bash_profile"
 
 # Install r
 brew install homebrew/science r
@@ -43,3 +73,13 @@ brew install vowpal-wabbit
 # Set desired colors for bash prompt
 echo -e "GREEN='\[$(tput setaf 47)\]'\nBLUE='\[$(tput setaf 33)\]'\nORANGE='\[$(tput setaf 214)'RESET='\[$(tput sgr0)\]'\nfunction parse_git_branch {\ngit branch --no-color 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/(\1)/'}" > ~/.bash_theme
 echo -e "source ~/.bash_theme" >> ~/.bash_profile
+
+# Install Atom
+wget https://github.com/atom/atom/releases/download/v1.13.1/atom-mac.zip
+unzip atom-mac.zip -d ~/Applications
+rm atom-mac.zip
+
+# Install Flux
+wget https://justgetflux.com/mac/Flux.zip
+unzip Flux.zip -d ~/Applications
+rm Flux.zip
